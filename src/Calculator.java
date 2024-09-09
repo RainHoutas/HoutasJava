492752eb1b17c3504b63a4e13e1f9812d819a4c8
RainHoutas <1271084579@qq.com>
2024-09-09 20:41:05 +0800

    第二次作业第二次提交

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 abstract class CalculationMethods {
     abstract double compute(int[] numbers);
 }
@@ -9,8 +11,7 @@ class Add extends CalculationMethods {
         for (int number : numbers) {
             sum += number;
         }
-        System.out.println("Sum: " + sum);
-        return 0;
+        return sum;
     }
 }
 
@@ -21,8 +22,7 @@ class Subtract extends CalculationMethods {
         for (int i = 1; i < numbers.length; i++) {
             difference -= numbers[i];
         }
-        System.out.println("Difference: " + difference);
-        return 0;
+        return difference;
     }
 }
 
@@ -33,8 +33,7 @@ class Multiply extends CalculationMethods {
         for (int number : numbers) {
             product *= number;
         }
-        System.out.println("Product: " + product);
-        return 0;
+        return product;
     }
 }
 
@@ -51,7 +50,53 @@ class Divide extends CalculationMethods {
 
 public class Calculator {
     public static void main(String[] args){
-        System.out.println("请选择计算方式：1.加法 2.减法 3.乘法 4.除法");
-        
+        double result=0;
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("请选择计算方式：1.加法 2.减法 3.乘法 4.除法");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            System.out.println("请输入数字个数：");
+            int n = scanner.nextInt();
+            if (result != 0) {
+                n++;
+            }
+            int[] numbers = new int[n];
+            System.out.println("请输入数字：");
+            int j = 0;
+            if (result != 0) {
+                numbers[0] = (int) result;
+                j = 1;
+            }
+            for (int i=j;i< n; i++) {
+                numbers[i] = scanner.nextInt();
+            }
+            switch (choice) {
+                case 1:
+                    CalculationMethods add = new Add();
+                    result += add.compute(numbers);
+                    break;
+                case 2:
+                    CalculationMethods subtract = new Subtract();
+                    result += subtract.compute(numbers);
+                    break;
+                case 3:
+                    CalculationMethods multiply = new Multiply();
+                    result += multiply.compute(numbers);
+                    break;
+                case 4:
+                    CalculationMethods divide = new Divide();
+                    result += divide.compute(numbers);
+                    break;
+                default:
+                    System.out.println("没有此选项，期待以后开发");
+            }
+            System.out.println("当前结果: " + result);
+            System.out.println("是否继续计算？1.是 2.否");
+            int isContinueChoice = scanner.nextInt();
+            if (isContinueChoice == 2) {
+                isContinue = false;
+            }
+        }
     }
 }
